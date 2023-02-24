5cacf0c6d45fb9b7b20c235f8dc0ae86bcae00ee
kimly.888 <kimly.jet@gmail.com>
2023-02-24 17:49:25 +0900

    Feat: Add getRandomPrompt function

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,1 +1,11 @@
 import { surpriseMePrompts } from "../constants";
+
+export function getRandomPrompt(prompt: string): string {
+  const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
+  const randomPrompt = surpriseMePrompts[randomIndex];
+
+  // Make sure user doesn't get same prompt consecutively
+  if (randomPrompt === prompt) return getRandomPrompt(prompt);
+
+  return randomPrompt;
+}
