7063ea2c3d2ec9b8f040a267cf33c390416063f4
kimly.888 <kimly.jet@gmail.com>
2023-02-27 01:26:39 +0900

    Feat: Connect OpenAI API with server

--- server/routes/dalleRoutes.ts
+++ server/routes/dalleRoutes.ts
@@ -1,8 +1,42 @@
 import express, { Express, Request, Response } from "express";
 import * as dotenv from "dotenv";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
 const router = express.Router();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+router.route("/").get((req: Request, res: Response) => {
+  res.status(200).json("Hello from DALL-E!");
+});
+
+router.route("/").post(async (req: Request, res: Response) => {
+  try {
+    const { prompt } = req.body;
+    console.log(prompt);
+
+    // Send user prompt to OpenAI
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    // Get b64 encoded image string from OpenAI API response
+    const image = aiResponse.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error || "Something went wront");
+  }
+});
 
 export default router;
