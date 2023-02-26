8bd72beb264f64faa11287a6e8803554694753bc
kimly.888 <kimly.jet@gmail.com>
2023-02-26 21:39:18 +0900

    Feat: Add layout for Create Post page
    - Add FormField component with prop types

--- client/src/components/FormField.tsx
+++ client/src/components/FormField.tsx
@@ -1,4 +1,56 @@
-const FormField = () => {
-  return <div>FormField</div>;
+type FormFieldProps = {
+  labelName: string;
+  type: string;
+  name: string;
+  placeholder: string;
+  value: any;
+  handleChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isSurpriseMe?: boolean;
+  handleSurpriseMe?: () => void;
+};
+
+const FormField = ({
+  labelName,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}: FormFieldProps) => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 mb-2">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {labelName}
+        </label>
+
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
+          >
+            Surprise me
+          </button>
+        )}
+      </div>
+
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#6469ff] focus:border-[#6469ff] outline-none block w-full p-3"
+      />
+    </div>
+  );
 };
 export default FormField;
